95a25e8c519141096d365f5a53a832974c0b6c87
Rokeeb01Maruf <dayorokeeb234@gmail.com>
2026-06-03 09:24:11 +0100

    Finished backend custom validation instead of djangos

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .models import User
 
 def signup(request):
     if request.method == "GET":
@@ -6,33 +8,49 @@ def signup(request):
     elif request.method == "POST":
         first_name = request.POST.get("first_name")
         last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         cpassword = request.POST.get("cpassword")
         terms = request.POST.get("terms")
+        role = request.POST.get("role")
+        print(terms)
         if not first_name:
             error = "First name is required"
             return render(request, "accounts/signup.html", {"error": error})
         elif not last_name:
             error = "Last name is required"
             return render(request, "accounts/signup.html", {"error" : error})
-        elif terms == "none":
+        elif not username:
+            error = "username is required"
+            return render(request, "accounts/signup.html", {"error" : error})
+        elif terms != "on":
             error = "Please read and accept our terms and condition"
             return render(request, "accounts/signup.html", {"error": error})
-        elif not "@" in email or not ".com" in email:
+        elif not "@" in email or not "." in email:
             return render(request, "accounts/signup.html", {"error": "not a valid email"})
-        elif not password and password != cpassword:
+        elif not password or password != cpassword:
             error = "Password Mismatch"
             return render(request, "accounts/signup.html", {"error" : error})
+        elif not role:
+            error = "No role selected"
+            return render(request, "accounts/signup.html", {"error" : error})
         else:
-            form = {
-                "first_name" : first_name,
-                "last_name" : last_name,
-                "email" : email,
-                "password" : password,
-                "terms" : terms
-            }
-        return render(request, "accounts/signup.html", form)
+            try:
+                user = User(
+                    first_name = first_name,
+                    last_name = last_name,
+                    username = username,
+                    email = email,
+                    role = role
+                )
+                user.set_password(password)
+                user.save()
+                login(request, user)
+                return redirect("signin")
+            except Exception as e:
+                error = "server error"
+                return render(request, "accounts/signup.html", {"error": str(e), "message": error})
 
 def signin(request):
     if request.method == "GET":
