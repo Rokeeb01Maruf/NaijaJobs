395a5b86d5ebbad640ef327a3542a053cf2cf52a
Rokeeb01Maruf <dayorokeeb234@gmail.com>
2026-06-05 10:55:26 +0100

    updated siginin from email to username

--- static/js/signin.js
+++ static/js/signin.js
@@ -2,7 +2,7 @@ const signin = document.querySelector("#Login")
 const email = document.querySelector(".inmail")
 const password = document.querySelector(".inpassword")
 signin.addEventListener("submit", (e)=>{
-    if(!email.value.includes("@") || !email.value.includes(".com")){
+    if(!email.value){
         e.preventDefault()
         const error = document.querySelector(".pemail")
         error.classList.add("display")
