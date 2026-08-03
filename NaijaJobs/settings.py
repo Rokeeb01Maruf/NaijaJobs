23854f69692bbe3ea33a575f39b1f270cf70d17e
Rokeeb Maruf <dayorokeeb234@gmail.com>
2026-08-03 19:08:54 +0100

    Update settings.py

--- NaijaJobs/settings.py
+++ NaijaJobs/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-HOST = https://naijajobs-v2na.onrender.com
-ALLOWED_HOSTS = ["*"]
+HOST = "https://naijajobs-v2na.onrender.com"
+ALLOWED_HOSTS = [HOST]
 
 # Application definition
 
