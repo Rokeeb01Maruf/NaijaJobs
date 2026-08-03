4fc8d4a1f01f82d9979f7a8f34c20c6e7faa15a8
Rokeeb Maruf <dayorokeeb234@gmail.com>
2026-08-03 15:37:54 +0100

    Update settings.py for security improvements
    Removed hardcoded secret key and allowed hosts for better security practices.

--- NaijaJobs/settings.py
+++ NaijaJobs/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
-generated_secret_key = "-_4l*hz+=%ej8-0eylne--wn-fbul2%3a)0+^ll_8$%3q$2x!&"
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", generated_secret_key)
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
